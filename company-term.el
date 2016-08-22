96f8de2534cc5303497efee584f8eff0144f45be
Aborn Jiang <loveaborn@foxmail.com>
2016-08-22 16:18:07 +0800

    add company-term

---
+++ company-term.el
@@ -0,0 +1,12 @@
+;;; company-term.el --- An company plug for multi-term  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2016 Aborn Jiang
+
+;;; Code:
+;; TODO
+
+(require 'cl-lib)
+(require 'company)
+
+(provide 'company-term)
+;;; company-term.el ends here
