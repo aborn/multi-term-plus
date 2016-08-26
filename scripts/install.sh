b1de9f119b54524201a573f943f48507d11bb61b
guobao.jiang <loveaborn@foxmail.com>
2016-08-26 10:28:10 +0800

    print pwd info

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,6 +1,8 @@
 main() {
+    echo "Current directory: `pwd`"
     DIRECTORY="~/multi-term-plus"
     if [ ! -d "$DIRECTORY" ]; then
+        echo "git clone https://github.com/aborn/multi-term-plus.git ${DIRECTORY}"
         git clone https://github.com/aborn/multi-term-plus.git $DIRECTORY
         echo "Add following code to your emacs init file:"
         echo " (add-to-list 'load-path \"~/multi-term-plus\")"
